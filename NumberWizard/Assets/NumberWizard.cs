adb93398937c11433b536e6752021db09c519fbc
Asep Irman <sibaturaspirman@gmail.com>
2019-01-17 14:18:48 +0700

    learn 1

---
+++ NumberWizard/Assets/NumberWizard.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour {
+
+    int max;
+    int min;
+    int guess;
+
+    // Use this for initialization
+    void Start () {
+        StartGame();
+	}
+
+    void StartGame(){
+        max = 1000;
+        min = 1;
+        guess = 500;
+
+        Debug.Log("Welcome Dude!");
+        Debug.Log("Pick a number");
+        Debug.Log("Highest Number is : " + max);
+        Debug.Log("Lower Number is : " + min);
+        Debug.Log("Tell me if your number is higher or lower  than " + guess);
+        Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
+        max = max + 1;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.UpArrow)){
+            min = guess;
+            NextGuess();
+        }
+        else if(Input.GetKeyDown(KeyCode.DownArrow)){
+            max = guess;
+            NextGuess();
+        }
+        else if(Input.GetKeyDown(KeyCode.Return)){
+            StartGame();
+            Debug.Log("Enter was pressed");
+        }
+	}
+
+    void NextGuess(){
+        guess = (max + min) / 2;
+        Debug.Log(guess);
+    }
+}
